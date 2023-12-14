ab69f56167a7e836f5e405cbfce7c3fad4f8a32e
Kevin Johnson <harlemworld1985@gmail.com>
2023-12-14 11:53:42 +0100

    styling for the gird cards

--- src/Component/Grid/ContactCardGrid.tsx
+++ src/Component/Grid/ContactCardGrid.tsx
@@ -1,7 +1,62 @@
+import {
+  Avatar,
+  Card,
+  CardContent,
+  CardHeader,
+  Grid,
+  List,
+  ListSubheader,
+  Typography,
+} from "@mui/material";
 import React from "react";
+import { contactData } from "../../Data/ContactData";
 
 function ContactCardGrid() {
-  return <div>ContactCardGrid</div>;
+  return (
+    <Grid container spacing={2} sx={{ width: 700 }}>
+      {contactData.map((contact) => {
+        return (
+          <Grid item key={contact.name}>
+            <Card sx={{ width: 300 }}>
+              <CardHeader
+                title={contact.name}
+                subheader={contact.role}
+                avatar={<Avatar>{contact.name?.substring(0, 1).toUpperCase() || "A"}</Avatar>}
+              />
+              <CardContent>
+                <Typography>
+                  <b>Start Date: {contact.startDate}</b>
+                </Typography>
+                <Typography>
+                  <b>Work Preference: {contact.preference}</b>
+                </Typography>
+                <List
+                  sx={{ listStyle: "list-item", listStyleType: "circle", paddingLeft: 2 }}
+                  subheader={
+                    <ListSubheader
+                      sx={{
+                        right: 16,
+                        position: "inherit",
+                        fontSize: "1.25rem",
+                        color: "black",
+                        paddingLeft: "0",
+                      }}
+                    >
+                      Skills:
+                    </ListSubheader>
+                  }
+                >
+                  {contact.skills?.map((skill) => {
+                    return <li style={{ paddingBottom: "2px" }}>{skill}</li>;
+                  })}
+                </List>
+              </CardContent>
+            </Card>
+          </Grid>
+        );
+      })}
+    </Grid>
+  );
 }
 
 export default ContactCardGrid;

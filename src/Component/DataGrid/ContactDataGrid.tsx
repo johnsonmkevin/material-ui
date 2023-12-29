4e20d205bceb79d10194447d7b83f2f990078d87
Kevin Johnson <harlemworld1985@gmail.com>
2023-12-29 15:10:43 +0100

    contact data grid

--- src/Component/DataGrid/ContactDataGrid.tsx
+++ src/Component/DataGrid/ContactDataGrid.tsx
@@ -1,7 +1,62 @@
-import React from "react";
+import { Theme, useTheme } from "@mui/material";
+import { DataGrid, GridRenderCellParams } from "@mui/x-data-grid";
+
+import React, { useCallback } from "react";
+import { contactData } from "../../Data/ContactData";
+
+const columns = (theme: Theme) => [
+  {
+    field: "name",
+    headerName: "Name",
+    minWidth: 150,
+    renderCell: (params: GridRenderCellParams) => {
+      return params.value as string;
+    },
+  },
+  {
+    field: "role",
+    headerName: "Role",
+    minWidth: 100,
+    renderCell: (params: GridRenderCellParams) => {
+      return params.value as string;
+    },
+  },
+  {
+    field: "skills",
+    headerName: "Skills",
+    minWidth: 150,
+
+    renderCell: (params: GridRenderCellParams) => {
+      console.log(params);
+      return (
+        <div style={{ color: theme.palette.primary.main }}>
+          {params.value ? params.value[0] : ""}
+        </div>
+      );
+    },
+  },
+  {
+    field: "startDate",
+    headerName: "Start Date",
+    minWidth: 120,
+    renderCell: (params: GridRenderCellParams) => {
+      return params.value as string;
+    },
+  },
+  {
+    field: "preference",
+    headerName: "workPreference",
+    minWidth: 150,
+    renderCell: (params: GridRenderCellParams) => {
+      return params.value as string;
+    },
+  },
+];
 
 function ContactDataGrid() {
-  return <div>ContactDataGrid</div>;
+  const theme = useTheme();
+  const rows = () => [...contactData];
+  return <DataGrid rows={rows()} columns={columns(theme)} rowHeight={120} />;
 }
 
 export default ContactDataGrid;
